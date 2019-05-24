9e61ed4983a579e4fb5aece4936a468412ebcd2f
jorgez93 <joluzaib@hotmail.com>
2019-05-24 14:20:51 -0500

    Parcelable

---
+++ MyApplication/app/src/main/java/com/example/myapplication/Parcelable.kt
@@ -0,0 +1,23 @@
+package com.example.dr.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.example.myapplication.R
+
+class Parcelable : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_parcelable)
+
+        val jorge: Usuario?=this.intent.getParcelableExtra<Usuario>("usuario")
+
+        Log.i("parcelable","Nombre ${jorge?.nombre}")
+        Log.i("parcelable","Edad ${jorge?.edad}")
+        Log.i("parcelable","Fecha Nacimiento ${jorge?.fechaNacimiento}")
+        Log.i("parcelable","Sueldo ${jorge?.sueldo}")
+    }
+
+
+}
