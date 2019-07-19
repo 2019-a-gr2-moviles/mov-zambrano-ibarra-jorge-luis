2f5500560e283a8bdb02e14f912e7834b27c4599
jorgez93 <joluzaib@hotmail.com>
2019-07-19 15:01:02 -0500

    Fragmentos

--- Parcelable/app/src/main/java/com/example/parcelable/Fragmento1.kt
+++ Parcelable/app/src/main/java/com/example/parcelable/Fragmento1.kt
@@ -55,7 +55,7 @@ class Fragmento1 : Fragment() {
         if (context is OnFragmentInteractionListener) {
             listener = context
         } else {
-            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
+            //throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
         }
     }
 
